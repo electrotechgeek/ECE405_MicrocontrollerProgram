48bca2aee6eaceeeff79a01fe1ed1a6362245fb1
Paul <pdutley@gmail.com>
2013-02-27 20:27:35 -0600

    Tried changing BAUD rate
    Tried to get serial transmission by changing baud rate. No success.

--- MicroProgram405/MicroProgram405/main.c
+++ MicroProgram405/MicroProgram405/main.c
@@ -10,10 +10,11 @@ void main(void)
 {
 	M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
 	
-	UART_CLOCK_Start();
+	UART_CmdReset();
+	//UART_CLOCK_Start();
 	UART_Start(UART_PARITY_NONE);
 	
-	 UART_CPutString("\r\nWelcome to PSoC UART test program. V1.1 \r\n");
+	UART_CPutString("\r\nWelcome to PSoC UART test program. V1.1 \r\n");
 	
 	// Insert your main routine code here.
 }
