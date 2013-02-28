29fa8adb5029881826a79940fb15d60163b4b7d9
Paul <pdutley@gmail.com>
2013-02-27 19:35:38 -0800

    Initial Commit
    Started Project

---
+++ MicroProgram405/MicroProgram405/main.c
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------------------
+// C main line
+//----------------------------------------------------------------------------
+
+#include <m8c.h>        // part specific constants and macros
+#include "PSoCAPI.h"    // PSoC API definitions for all User Modules
+
+
+void main(void)
+{
+	M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
+	
+	UART_CLOCK_Start();
+	UART_Start(UART_PARITY_NONE);
+	
+	 UART_CPutString("\r\nWelcome to PSoC UART test program. V1.1 \r\n");
+	
+	// Insert your main routine code here.
+}
